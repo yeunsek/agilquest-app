18055d37fd8108ababb6d0873dc6493e2e8a6c77
yeunsek <yeunsek95@gmail.com>
2020-02-06 23:18:54 -0500

    .gitignore created

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,48 @@
-const reservationForm = document.querySelector('form')
-const reservationList = document.querySelector('table')
-const name = document.querySelector('input[name ="name"]')
-const loc = document.querySelector('input[name ="location"]')
-const startDate = document.querySelector('input[name="startDate"]')
-const endDate = document.querySelector('input[name="endDate"]')
-const messageOne = document.querySelector('#message-1')
+const reservationForm = document.querySelector("form");
+const reservationList = document.querySelector("table");
+const name = document.querySelector('input[name ="name"]');
+const loc = document.querySelector('input[name ="location"]');
+const startDate = document.querySelector('input[name="startDate"]');
+const endDate = document.querySelector('input[name="endDate"]');
+const messageOne = document.querySelector("#message-1");
 
 // messageOne.textContent = 'From JavaScript'
 
-reservationForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const reservationName = name.value
-    const reservationLocation = loc.value
-    const reservationStartDate = startDate.value
-    const reservationEndDate = endDate.value
-  
-    messageOne.textContent = 'Loading...'
+reservationForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const reservationName = name.value;
+  const reservationLocation = loc.value;
+  const reservationStartDate = startDate.value;
+  const reservationEndDate = endDate.value;
 
-    fetch('http://localhost:3000/reservation?name='+ encodeURIComponent(reservationName) + 
-    '&location='+encodeURIComponent(reservationLocation) +
-        '&startDate='+encodeURIComponent(reservationStartDate)+'&endDate=' + 
-            encodeURIComponent(reservationEndDate), {
-                method: 'post',
-                body: JSON.stringify({
-                    name: reservationName,
-                    location: reservationLocation,
-                    startDate: reservationStartDate,
-                    endDate: reservationEndDate
-                }),
-                headers: {'Content-Type':'application/json'}
-            }).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-           messageOne.textContent = data.error
-        } else {
-            messageOne.textContent = data.message
-        }
-    })
-})
-})
+  messageOne.textContent = "Loading...";
+
+  fetch(
+    "http://localhost:3000/reservation?name=" +
+      encodeURIComponent(reservationName) +
+      "&location=" +
+      encodeURIComponent(reservationLocation) +
+      "&startDate=" +
+      encodeURIComponent(reservationStartDate) +
+      "&endDate=" +
+      encodeURIComponent(reservationEndDate),
+    {
+      method: "post",
+      body: JSON.stringify({
+        name: reservationName,
+        location: reservationLocation,
+        startDate: reservationStartDate,
+        endDate: reservationEndDate
+      }),
+      headers: { "Content-Type": "application/json" }
+    }
+  ).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.message;
+      }
+    });
+  });
+});
--- src/utils/reservation.js
+++ src/utils/reservation.js
@@ -1,42 +1,49 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const addReservation = (location, name, startDate, endDate) => {
-    const reservations = loadReservations()
-    const id = 1
-    const duplicateReservation = reservations.find((reservation) => 
-        (reservation.name.toLowerCase() === name.toLowerCase() && reservation.location.toLowerCase() === location.toLowerCase() && reservation.startDate === startDate && reservation.endDate === endDate)
-    )
-    if (!duplicateReservation) {
-        reservations.push({
-            id: id + reservations.length,
-            location,
-            name,
-            startDate,
-            endDate
-        })
-        saveReservations(reservations)
-        return {message:"Reserved"}
-    } else {
-        return {message: "Duplicated Reservation"}
-    }
-}
+  const reservations = loadReservations();
+  const id = 1 + reservations.length;
+  const reservationStartDate = new Date(startDate).toLocaleString()
+  const reservationEndDate = new Date(endDate).toLocaleString()
+  const duplicateReservation = reservations.find(
+    reservation =>
+      reservation.name.toLowerCase() === name.toLowerCase() &&
+      reservation.location.toLowerCase() === location.toLowerCase() &&
+      reservation.startDate === reservationStartDate &&
+      reservation.endDate === reservationEndDate
+  );
+  
+  if (!duplicateReservation) {
+    reservations.push({
+      id: id,
+      location,
+      name,
+      startDate: reservationStartDate,
+      endDate: reservationEndDate
+    });
+    saveReservations(reservations);
+    return { message: "Reserved" };
+  } else {
+    return { message: "Duplicated Reservation" };
+  }
+};
 
-const saveReservations = (reservations) => {
-    const dataJSON = JSON.stringify(reservations)
-    fs.writeFileSync('reservations.json', dataJSON)
-}
+const saveReservations = reservations => {
+  const dataJSON = JSON.stringify(reservations);
+  fs.writeFileSync("reservations.json", dataJSON);
+};
 
 const loadReservations = () => {
-    try {
-        const dataBuffer = fs.readFileSync('reservations.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
-    }
-}
+  try {
+    const dataBuffer = fs.readFileSync("reservations.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
 
 module.exports = {
-    addReservation,
-    loadReservations
-}
+  addReservation,
+  loadReservations
+};
